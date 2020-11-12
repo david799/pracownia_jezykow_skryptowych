ee9254c03116bd663ed0f053f3a2b6ed33c37d3c
david799 <dawidstec96@gmail.om>
2020-11-12 22:33:55 +0100

    text replaceing added

--- skrypt_adobe_indesign/text_replacement.py
+++ skrypt_adobe_indesign/text_replacement.py
@@ -3,21 +3,6 @@ import random
 import string
 import xlrd
 
-"""workbook = xlrd.open_workbook('C:\\Users\\atheelm\\Documents\\python excel mission\\errors1.xlsx')
-workbook = xlrd.open_workbook('C:\\Users\\atheelm\\Documents\\python excel mission\\errors1.xlsx', on_demand = True)
-worksheet = workbook.sheet_by_index(0)
-first_row = [] # The row where we stock the name of the column
-for col in range(worksheet.ncols):
-    first_row.append( worksheet.cell_value(0,col) )
-# tronsform the workbook to a list of dictionnary
-data =[]
-for row in range(1, worksheet.nrows):
-    elm = {}
-    for col in range(worksheet.ncols):
-        elm[first_row[col]]=worksheet.cell_value(row,col)
-    data.append(elm)
-print data"""
-
 
 class InDesignTextReplacement():
     app = win32com.client.Dispatch('InDesign.Application.CC.2019')
@@ -27,14 +12,22 @@ class InDesignTextReplacement():
         self.indesign_file_path = indesign_file_path
         self.excel_file_path = excel_file_path
         self.indesign_document = app.Open(indesign_file_path)
-        self.excel_file = workbook = xlrd.open_workbook(self.excel_file_path)
-        translation_texts = docx_file.get_translate_texts()
-        i = 0
+        self.excel_file = xlrd.open_workbook(self.excel_file_path)
+        self.texts = []
 
-        for it in indesign_document.TextFrames:
-            print("weszlo")
-            if translation_texts[i][0] in (it.Contents):
-                app.FindTextPreferences.FindWhat = translation_texts[i][0]
-                app.ChangeTextPreferences.ChangeTo = translation_texts[i][1]
-                it.ChangeText()
-            i = i + 1
+    def set_texts(self):
+        worksheet = self.excel_file.sheet_by_index(0)
+        first_column_values = worksheet.col_values(0)
+        second_column_values = worksheet.col_values(1)
+        self.texts = [(first_column_values[i], second_column_values[i]) for i in range(len(first_column_values))]
+
+    def sort_texts(self):
+        self.texts.sort(key = lambda x: x[0], reverse=True)   
+
+    def replace_texts(self):
+        for text_tuple in self.texts:
+            for text_frame in indesign_document.TextFrames:
+                if text_tuple[0] in (text_frame.Contents):
+                    app.FindTextPreferences.FindWhat = text_tuple[0]
+                    app.ChangeTextPreferences.ChangeTo = text_tuple[1]
+                    text_frame.ChangeText()
