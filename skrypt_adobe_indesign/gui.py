a053cbfb2e610728ce1608dba14014131dc58c2e
david799 <dawidstec96@gmail.om>
2020-11-19 09:29:55 +0100

    gui connected with replacing script

--- skrypt_adobe_indesign/gui.py
+++ skrypt_adobe_indesign/gui.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
+from text_replacement import InDesignTextReplacement
 
 class IndesignReplaceApp(tk.Frame):
+    SKIP_WHITESPACES_TEXT = "Skip white spaces"
 
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
@@ -24,7 +26,7 @@ class IndesignReplaceApp(tk.Frame):
 
         self.choose_indd = tk.Button(self)
         self.choose_indd.grid(row=0, column=1)
-        self.choose_indd["text"] = "Choose indd"
+        self.choose_indd["text"] = "Choose idml"
         self.choose_indd["command"] = self.choose_indd_file
 
         self.choose_excel = tk.Button(self)
@@ -34,11 +36,11 @@ class IndesignReplaceApp(tk.Frame):
 
         self.white_spaces_label = tk.Label(self)
         self.white_spaces_label.grid(row=3)
-        self.white_spaces_label["text"] = "Skip white spaces"
+        self.white_spaces_label["text"] = IndesignReplaceApp.SKIP_WHITESPACES_TEXT + " (Yes)"
 
         self.white_spaces_toggle = tk.Button(self)
         self.white_spaces_toggle.grid(row=3, column=1)
-        self.white_spaces_toggle["text"] = "Yes"
+        self.white_spaces_toggle["text"] = "No"
         self.white_spaces_toggle["command"] = self.white_spaces_change_toggle
 
         # just to make space
@@ -46,7 +48,7 @@ class IndesignReplaceApp(tk.Frame):
 
         self.warning_label = tk.Label(self)
         self.warning_label.grid(row=5)
-        self.warning_label["text"] = "Not replaced: 5/150"
+        self.warning_label["text"] = "Not replaced: 0/0"
 
         self.start_replacing = tk.Button(self)
         self.start_replacing.grid(row=5, column=1)
@@ -63,12 +65,19 @@ class IndesignReplaceApp(tk.Frame):
 
     def white_spaces_change_toggle(self):
         self.skip_white_spaces = not self.skip_white_spaces
-        self.white_spaces_toggle["text"] = "Yes" if self.skip_white_spaces else "No"
+        self.white_spaces_toggle["text"] = "No" if self.skip_white_spaces else "Yes"
+        self.white_spaces_label["text"] = IndesignReplaceApp.SKIP_WHITESPACES_TEXT + (" (Yes)" if self.skip_white_spaces else " (No)")
 
     def start_replacing_process(self):
         self.indd_file_path = self.indd_entry_text.get()
         self.excel_file_path = self.excel_entry_text.get()
-        print("start")
+        replacement_obj = InDesignTextReplacement(self.indd_file_path, self.excel_file_path, self.skip_white_spaces)
+        replacement_obj.set_up()
+        self.start_replacing.config(state="disabled")
+        completed, excel_rows, used_rows = replacement_obj.replace_texts()
+        if completed:
+            self.start_replacing.config(state="normal")
+        self.warning_label["text"] = "Not replaced: " + str(used_rows) + "/" + str(excel_rows)
 
 root = tk.Tk()
 app = IndesignReplaceApp(master=root)
--- skrypt_adobe_indesign/text_replacement.py
+++ skrypt_adobe_indesign/text_replacement.py
@@ -1,33 +1,88 @@
-import win32com.client
+from os import listdir
+from os.path import isfile, join
+from xlutils.copy import copy
+import xml.etree.ElementTree as ET
 import random
 import string
 import xlrd
+import xlwt
+
+from simple_idml import idml
+from simple_idml.extras import create_idml_package_from_dir
 
 
 class InDesignTextReplacement():
-    app = win32com.client.Dispatch('InDesign.Application.CC.2019')
-    app.scriptPreferences.userInteractionLevel = 1699640946
-
     def __init__(self, indesign_file_path, excel_file_path, skip_white_spaces):
         self.indesign_file_path = indesign_file_path
         self.excel_file_path = excel_file_path
-        self.indesign_document = app.Open(indesign_file_path)
+        self.indesign_document = idml.IDMLPackage(indesign_file_path)
         self.excel_file = xlrd.open_workbook(self.excel_file_path)
-        self.texts = []
+        self.skip_white_spaces = skip_white_spaces
+        self.text_frames_contents = []
+        self.excel_texts = []
 
     def set_texts(self):
         worksheet = self.excel_file.sheet_by_index(0)
         first_column_values = worksheet.col_values(0)
         second_column_values = worksheet.col_values(1)
-        self.texts = [(first_column_values[i], second_column_values[i]) for i in range(len(first_column_values))]
+        self.excel_texts = [[first_column_values[i], second_column_values[i], 0, i] for i in range(len(first_column_values))]
 
     def sort_texts(self):
-        self.texts.sort(key = lambda x: x[0], reverse=True)   
+        self.excel_texts.sort(key = lambda x: x[0])
+
+    def set_up(self):
+        self.set_texts()
+        self.sort_texts()
+
+    def create_idml_package(self, path):
+        create_idml_package_from_dir(path, path + '_new.idml')
 
     def replace_texts(self):
-        for text_tuple in self.texts:
-            for text_frame in indesign_document.TextFrames:
-                if text_tuple[0] in (text_frame.Contents):
-                    app.FindTextPreferences.FindWhat = text_tuple[0]
-                    app.ChangeTextPreferences.ChangeTo = text_tuple[1]
-                    text_frame.ChangeText()
+        extract_to = './' + self.indesign_file_path.split('.')[-2].split('/')[-1]
+        self.indesign_document.extractall(extract_to)
+        stories_path = join(extract_to, './Stories')
+        story_files = [join(stories_path, f) for f in listdir(stories_path) if isfile(join(stories_path, f))]
+        xmls_and_paths = []
+
+        for story_file in story_files:
+            tree = ET.parse(story_file)
+            xmls_and_paths.append(
+                {
+                    "xml": tree,
+                    "path": story_file
+                }
+            )
+            root = tree.getroot()
+            for content in root.iter('Content'):
+                self.text_frames_contents.append(content)
+
+        for text_tuple in self.excel_texts:
+            for content in self.text_frames_contents:
+                if text_tuple is not None and content is not None:
+                    if content.text is not None:
+                        if self.skip_white_spaces:
+                            text_frame_without_spaces = " ".join(content.text.split())
+                            excel_text_without_spaces = " ".join(text_tuple[0].split())
+                            if excel_text_without_spaces in text_frame_without_spaces:
+                                content.text = content.text.replace(text_tuple[0], text_tuple[1])
+                                text_tuple[2] = 1
+                        else:
+                            if text_tuple[0] in (content.text):
+                                content.text = content.text.replace(text_tuple[0], text_tuple[1])
+                                text_tuple[2] = 1
+
+        for xml_and_path in xmls_and_paths:
+            xml = xml_and_path["xml"]
+            path = xml_and_path["path"]
+            xml.write(path)
+        self.create_idml_package(extract_to)
+
+        writable_excel_copy = copy(self.excel_file)
+        writable_sheet = writable_excel_copy.get_sheet(0)
+        st = xlwt.easyxf('pattern: pattern solid, fore_colour red;')
+        for text_tuple in self.excel_texts:
+            if not text_tuple[2]:
+                writable_sheet.write(text_tuple[3], 0, text_tuple[0], st)
+        writable_excel_copy.save("not_replaced_texts_underlined.xls")
+
+        return True, len(self.excel_texts), sum([text_tuple[2] for text_tuple in self.excel_texts])
