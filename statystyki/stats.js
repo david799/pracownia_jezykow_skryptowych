c0b48196affa041294a256067e714fba67541f93
david799 <dawidstec96@gmail.com>
2021-01-27 21:16:54 +0100

    sqlite library changed, and structure of project changed

--- statystyki/stats.js
+++ statystyki/stats.js
@@ -1,36 +1,53 @@
-const sqlite3 = require('sqlite3').verbose();
+const Database = require('better-sqlite3')
 
-let db = new sqlite3.Database('stats.db');
+class RestaurantStatsData {
+  orderNotSupportedQuery = "SELECT 'SPECIFIED ORDER VALUE IS WRONG'"
+  isOrderSupported = function (order) {
+    if (['ASC', 'DESC'].includes(order))
+      return true
+    return false
+  }
 
-function topEarnerMeals(){
-  let sql = `SELECT meal_name, SUM(meal_price) earned FROM meals GROUP BY meal_name ORDER BY earned DESC;`;
-  let topEarners = []
-  
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Top earner meals")
-    console.log(rows)
-  });
-  return topEarners;
+  mealsEarningsSQL = function (order, number_of_rows) {
+    if (this.isOrderSupported(order))
+      return `SELECT meal_name, SUM(meal_price) earned FROM meals GROUP BY meal_name ORDER BY earned ${order} LIMIT ${number_of_rows};`
+    else 
+      return this.orderNotSupportedQuery
+  }
+
+  categoriesEarningsSQL = function (order, number_of_rows) {
+    if (this.isOrderSupported(order))
+      return `SELECT category_name, SUM(meal_price) earned FROM meals GROUP BY meal_name ORDER BY earned ${order} LIMIT ${number_of_rows};`
+    else 
+      return this.orderNotSupportedQuery
+  }
+
+  constructor(dbPath, amountOfData) {
+    this.db = new Database(dbPath, { verbose: console.log })
+    this.amountOfData = amountOfData
+  }
+
+  getQueryResults(query) {
+    const stmt = this.db.prepare(query)
+    const rows = stmt.all()
+    return rows
+  }
+
+  get topEarnerMeals(){
+    return this.getQueryResults(this.mealsEarningsSQL("DESC", this.amountOfData))
+  }
+  get worstEarnerMeals(){
+    return this.getQueryResults(this.mealsEarningsSQL("ASC", this.amountOfData))
+  }
 }
 
-function worstEarnerMeals(){
-  let sql = `SELECT meal_name, SUM(meal_price) earned FROM meals GROUP BY meal_name ORDER BY earned ASC;`;
-  let topEarners = []
-  
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Worst earner meals")
-    console.log(rows)
-  });
-  return topEarners;
-}
+const stats = new RestaurantStatsData('stats.db', 3)
+console.log("Top earning meals")
+console.log(stats.topEarnerMeals)
+console.log("Worst earning meals")
+console.log(stats.worstEarnerMeals)
 
-topEarnerMeals()
-worstEarnerMeals()
 
-db.close();
+
+
+
